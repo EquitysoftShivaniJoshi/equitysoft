b24aea17206d7e3d50981c0235faef3db50224fb
Equitysoft Technologies <shivani.joshi@equitysoft.in>
2025-06-24 11:48:21 +0530

    Add file changes 1

--- wp-content/themes/equitysoft/template-parts/content-none.php
+++ wp-content/themes/equitysoft/template-parts/content-none.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<section class="no-results not-found test">
+<section class="no-results not-found test1">
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'equitysoft' ); ?></h1>
 	</header><!-- .page-header -->
