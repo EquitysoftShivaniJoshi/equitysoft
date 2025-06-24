7417bee572745b37a0aca7aeba9a10bc7757ccae
Equitysoft Technologies <shivani.joshi@equitysoft.in>
2025-06-24 11:29:38 +0530

    Add theme folder

---
+++ wp-content/themes/equitysoft/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * equitysoft Theme Customizer
+ *
+ * @package equitysoft
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function equitysoft_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'equitysoft_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'equitysoft_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'equitysoft_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function equitysoft_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function equitysoft_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function equitysoft_customize_preview_js() {
+	wp_enqueue_script( 'equitysoft-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'equitysoft_customize_preview_js' );
